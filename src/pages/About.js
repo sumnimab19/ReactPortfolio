01d14e48fb87da7298826b9dc9637059147c666d
Sumnima B <sumnimabhandari@gmail.com>
2020-09-06 13:18:50 -0700

    Updated content on about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,11 +11,13 @@ function About() {
         className = "myPic"/>
         
         <p class="myParagraph">
-            Welcome to my page. I am an experienced Software Quality Assurance Analyst who has 4+ years of testing experience in HealthCare Software. I am currently enrolled in University of Arizona Coding Bootcamp. I would like to build a stunning website for my Dad and some 
-            useful apps for myself with the help of the knowledge I gather from this bootcamp.
+            Welcome to Sumnima's page. Sumnima is an aspiring full stack web developer and an experienced Software Quality Assurance Analyst / Epic Application Analyst. She believes in continuous learning.
+            She is currently enrolled in University of Arizona Coding Bootcamp to learn the latest coding technologies and utilize those to solve problems. 
+            Sumnima lives in Oro Valley, Arizona with her husband and their daughter.
+        
         </p>
         <p class="myParagraph">
-            I love biking, hiking, volley ball, gardening, watching movies, playing games with my family in free time. 
+            She loves biking, hiking, volley ball, watching movies, playing games with her family in free time. 
          </p>
     </div>
   );
